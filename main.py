f0c58107fc0d20eaa56f7c513abf41dd984bcb76
beannama <ben.nar.95@gmail.com>
2025-12-09 11:21:01 -0300

    Created gitignore, added main functionality

--- main.py
+++ main.py
@@ -11,3 +11,29 @@ Responsibilities:
 This file contains no business logic.
 
 '''
+import argparse
+import os
+import json
+
+
+def parse_args():
+    p = argparse.ArgumentParser(description="Tiny SOAR Pipeline")
+    p.add_argument("--input", "-i", help="Alert JSON path")
+    p.add_argument("--outdir", "-o", default="output")
+    p.add_argument("--sample", action="store_true")
+    return p.parse_args()
+
+
+from SOAR.Ingest.loader import load_alert
+
+def main():
+    args = parse_args()
+    os.makedirs(args.outdir, exist_ok=True)
+
+    alert = load_alert(path=args.input, use_sample=args.sample)
+
+    pretty_print = json.dumps(alert, indent=2)
+    print(pretty_print)
+
+if __name__ == "__main__":
+    main()
